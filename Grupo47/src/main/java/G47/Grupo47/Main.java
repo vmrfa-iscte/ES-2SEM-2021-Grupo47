3aaa1e2e659b00f706d6d1106b983a0327b3ee10
tasmm-iscte <tasmm@iscte-iul.pt>
2021-04-01 14:20:02 +0100

    Atualização Classe main

--- Grupo47/src/main/java/G47/Grupo47/Main.java
+++ Grupo47/src/main/java/G47/Grupo47/Main.java
@@ -1,12 +1,13 @@
 package G47.Grupo47;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class Main {
 	
-	public static void main(String[] args) {
-		File projectDir = new File("C:\\Users\\rui.fontoura\\Downloads\\jasml_0.10\\src\\com\\jasml");
+	public static void main(String[] args) throws IOException {
+		File projectDir = new File("C:\\Users\\Tomás Mendes\\Desktop\\jasml_0.10\\src\\com\\jasml");
 		DirExplorer de = new DirExplorer(new LOC_methodHandler());
 		de.explore(projectDir);
 		ArrayList<Metricas> metricas = de.getMetricas();
@@ -16,9 +17,12 @@ public class Main {
 		DirExplorer de2 = new DirExplorer(new LOC_classHandler(),metricas);
 		de2.explore(projectDir);
 		ArrayList<Metricas> metricas_LOC_class = de2.getMetricas();
-		for(Metricas m: metricas_LOC_class) {
-			System.out.println(m.toString());
-		}
+//		for(Metricas m: metricas_LOC_class) {
+//			System.out.println(m.toString());
+//		}
+		ExcelManip manip = new ExcelManip();
+		System.out.println("Excel criado");
+		manip.createExcel(manip.extractHeaders(),metricas_LOC_class);
 	}
 
 }
