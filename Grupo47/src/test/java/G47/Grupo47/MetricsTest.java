159384323414f41c93efd39b216a63f4c8d83174
gtggt <gtggt@iscte-iul.pt>
2021-04-04 22:39:55 +0100

    Metrics 75 % coberto

--- Grupo47/src/test/java/G47/Grupo47/MetricsTest.java
+++ Grupo47/src/test/java/G47/Grupo47/MetricsTest.java
@@ -3,41 +3,58 @@ package G47.Grupo47;
 import junit.framework.TestCase;
 
 public class MetricsTest extends TestCase {
-	
+
 	private Metrics n;
 
-
 	public void testConstructor() {
-		n = new Metrics("x","y");
+		n = new Metrics("x", "y");
 		assertNotNull(n);
-		assertEquals("x",n.getClasse());
-		assertEquals("y",n.getPacote());
+		assertEquals("x", n.getClasse());
+		assertEquals("y", n.getPacote());
 	}
-	
+
 	public void testConstructor2() {
-		n= new Metrics("x","y","z",4);
+		n = new Metrics("x", "y", "z", 4);
 		assertNotNull(n);
-		assertEquals("x",n.getNome_metodo());
-		assertEquals("y",n.getClasse());
-		assertEquals("z",n.getPacote());
-		assertEquals(4,n.getLOC_method());		
+		assertEquals("x", n.getNome_metodo());
+		assertEquals("y", n.getClasse());
+		assertEquals("z", n.getPacote());
+		assertEquals(4, n.getLOC_method());
 	}
-	
+
 	public void testConstructor3() {
-		n= new Metrics("x","y","z",1,2,3,4,5);
+		n = new Metrics("x", "y", "z", 1, 2, 3, 4, 5);
 		assertNotNull(n);
-		assertEquals("x",n.getNome_metodo());
-		assertEquals("y",n.getClasse());
-		assertEquals("z",n.getPacote());
-		assertEquals(1,n.getLOC_method());	
-		assertEquals(2,n.getLOC_class());
-		assertEquals(3,n.getCYCLO_method());
-		assertEquals(4,n.getNOM_class());	
-		assertEquals(5,n.getWMC_class());	
+		assertEquals("x", n.getNome_metodo());
+		assertEquals("y", n.getClasse());
+		assertEquals("z", n.getPacote());
+		assertEquals(1, n.getLOC_method());
+		assertEquals(2, n.getLOC_class());
+		assertEquals(3, n.getCYCLO_method());
+		assertEquals(4, n.getNOM_class());
+		assertEquals(5, n.getWMC_class());
 	}
 	
-	
-	
+	public void testSetters() {
+		n = new Metrics("","","",0,0,0,0,0);
+		n.setLOC_method(1);
+		assertEquals(1,n.getLOC_method());
+		n.setClasse("classe");
+		assertEquals("classe",n.getClasse());
+		n.setCYCLO_method(2);
+		assertEquals(2,n.getCYCLO_method());
+		n.setLOC_class(3);
+		assertEquals(3,n.getLOC_class());
+		n.setWMC_class(5);
+		assertEquals(5,n.getWMC_class());
+		n.setPacote("package");
+		assertEquals("package",n.getPacote());
+		n.setNome_metodo("metodo");
+		assertEquals("metodo",n.getNome_metodo());
+		n.setNOM_class(6);
+		assertEquals(6,n.getNOM_class());
+		
+	}
 	
 
 }
