47ed5214ab3d2ebbc3fb7ccfa3fc20f07d312863
vmrfa-iscte <vmrfa@iscte-iul.pt>
2021-04-09 17:13:03 +0100

    Atualização Caracteristicas na GUI.

--- Grupo47/src/main/java/G47/Grupo47/GuiClass.java
+++ Grupo47/src/main/java/G47/Grupo47/GuiClass.java
@@ -4,12 +4,18 @@ package G47.Grupo47;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map.Entry;
 
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JList;
 import javax.swing.JTextField;
 import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
 
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Display;
@@ -26,6 +32,11 @@ import org.eclipse.swt.widgets.Text;
 import org.eclipse.swt.widgets.List;
 import org.eclipse.swt.custom.StyledText;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Table;
+import org.eclipse.swt.graphics.Point;
+import org.eclipse.swt.widgets.TableColumn;
+import org.eclipse.swt.events.MouseAdapter;
+import org.eclipse.swt.events.MouseEvent;
 
 public class GuiClass extends Shell {
 	
@@ -35,34 +46,29 @@ public class GuiClass extends Shell {
 	private List ficheirosexcel;
 	private Text foldername;
 	private StyledText styledText;
-	private Text text;
 	private Composite composite;
 	private Button btnDefinirRegras;
 	private Text limite_1;
 	private Text limite_2;
 	private Text codesmells;
+	String nomepath = new String();
+	File selectedFile1 = null;
+	String nameFile = "";
+	private Text txtNmeroDeMtodos;
+	private Text NumClasses;
+	private Text txtNmeroDeClasses;
+	private Text txtNmeroDeLinhas;
+	private Text txtNmeroDePackages;
+	private Text NumPackages;
+	private Text NumMethods;
+	private Text NumLines;
+	private HashMap<String,ArrayList<String>> mapStats = new HashMap<>();
 
 	/**
 	 * Launch the application.
 	 * 
 	 * @param args
 	 */
-	public static void main(String args[]) {
-		try {
-			
-			Display display = Display.getDefault();
-			GuiClass shell = new GuiClass(display);
-			shell.open();
-			shell.layout();
-			while (!shell.isDisposed()) {
-				if (!display.readAndDispatch()) {
-					display.sleep();
-				}
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
 
 	/**
 	 * Create the shell.
@@ -77,16 +83,11 @@ public class GuiClass extends Shell {
 		foldername = new Text(this, SWT.BORDER);
 		foldername.setBounds(10, 14, 345, 26);
 
-		text = new Text(this, SWT.BORDER);
-		text.setBounds(372, 58, 351, 187);
-
 		Button pasta = new Button(this, SWT.NONE);
 		pasta.addSelectionListener(new SelectionAdapter() {
 			@Override
 			public void widgetSelected(SelectionEvent e) {
 				JFileChooser pathpasta = new JFileChooser(".");
-				String nomepath = new String();
-				File selectedFile1 = null;
 				pathpasta.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
 				int returnValue = pathpasta.showOpenDialog(null);
 				if (returnValue == JFileChooser.APPROVE_OPTION) {
@@ -101,27 +102,118 @@ public class GuiClass extends Shell {
 		pasta.setText("Selecionar pasta");
 
 		ficheirosexcel = new List(this, SWT.BORDER);
-		ficheirosexcel.addSelectionListener(new SelectionAdapter() {
-			public void valueSelected(ListSelectionEvent e) {
+		ficheirosexcel.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseDoubleClick(MouseEvent e) {
 				indice = ficheirosexcel.getSelectionIndex();
-				if (!e.getValueIsAdjusting()) {
+				System.out.println("indice: "+indice);
+				
 					if (indice != -1) {
-						// text.setText("numero total de packages:");
-						// preencher lista da direita com as caracteristicas gerais do projeto de acordo
-						// com o ficheiro selecionado ( indice )
+						 for (Entry<String, ArrayList<String>> entry : mapStats.entrySet()) {
+							 System.out.println(ficheirosexcel.getItem(indice));
+							 if(entry.getKey().equals(ficheirosexcel.getItem(indice))) {
+								 ArrayList<String> statsToWrite = entry.getValue();
+								 NumLines.setText(statsToWrite.get(0));
+								 NumClasses.setText(statsToWrite.get(2));
+								 NumMethods.setText(statsToWrite.get(1));
+								 NumPackages.setText(statsToWrite.get(3));
+							 }
+						 }
 
 					}
-				}
+				
+				
 			}
 		});
+//		ficheirosexcel.addSelectionListener(new ListSelectionListener() {
+//			public void valueChanged(ListSelectionEvent e) {
+//				indice = ficheirosexcel.getSelectionIndex();
+//				System.out.println("indice: "+indice);
+//				if (!e.getValueIsAdjusting()) {
+//					if (indice != -1) {
+//						 for (Entry<String, ArrayList<String>> entry : mapStats.entrySet()) {
+//							 System.out.println(ficheirosexcel.getItem(indice));
+//							 if(entry.getKey().equals(ficheirosexcel.getItem(indice))) {
+//								 ArrayList<String> statsToWrite = entry.getValue();
+//								 NumLines.setText(statsToWrite.get(0));
+//								 NumClasses.setText(statsToWrite.get(1));
+//								 NumMethods.setText(statsToWrite.get(2));
+//								 NumPackages.setText(statsToWrite.get(3));
+//							 }
+//						 }
+//
+//					}
+//				}
+//			}
+//		});
 		ficheirosexcel.setBounds(10, 57, 345, 188);
-
-		Button extrair = new Button(this, SWT.NONE);
-		extrair.setBounds(544, 12, 179, 30);
-		extrair.setText("Extrair métricas");
 
 		composite = new Composite(this, SWT.NONE);
 		composite.setBounds(10, 261, 713, 146);
+		
+		txtNmeroDeMtodos = new Text(this, SWT.BORDER);
+		txtNmeroDeMtodos.setText("Número de Métodos");
+		txtNmeroDeMtodos.setBounds(372, 160, 152, 26);
+		
+		NumClasses = new Text(this, SWT.BORDER);
+		NumClasses.setBounds(544, 58, 78, 26);
+		
+		txtNmeroDeClasses = new Text(this, SWT.BORDER);
+		txtNmeroDeClasses.setText("Número de Classes");
+		txtNmeroDeClasses.setBounds(372, 58, 152, 26);
+		
+		txtNmeroDeLinhas = new Text(this, SWT.BORDER);
+		txtNmeroDeLinhas.setText("Número de Linhas");
+		txtNmeroDeLinhas.setBounds(372, 219, 152, 26);
+		
+		txtNmeroDePackages = new Text(this, SWT.BORDER);
+		txtNmeroDePackages.setText("Número de Packages");
+		txtNmeroDePackages.setBounds(372, 107, 152, 26);
+		
+		NumPackages = new Text(this, SWT.BORDER);
+		NumPackages.setBounds(544, 107, 78, 26);
+		
+		NumMethods = new Text(this, SWT.BORDER);
+		NumMethods.setBounds(544, 160, 78, 26);
+		
+		NumLines = new Text(this, SWT.BORDER);
+		NumLines.setBounds(544, 219, 78, 26);
+		
+		
+		Button extrair = new Button(this, SWT.NONE);
+		extrair.setBounds(544, 12, 179, 30);
+		extrair.setText("Extrair métricas");
+		extrair.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				DirExplorer dirEx = new DirExplorer(selectedFile1);
+				try {
+					ArrayList<Metrics> metrics = dirEx.explore();
+					ExcelManip em = new ExcelManip(selectedFile1);
+					em.createExcel(em.extractHeaders(), metrics);
+					Statistics stats = new Statistics(metrics);
+					ArrayList<String> StringStats = new ArrayList<>();
+					StringStats.add(String.valueOf(stats.countLinesOfCode()));
+					StringStats.add(String.valueOf(stats.countNumberOfMethods()));
+					StringStats.add(String.valueOf(stats.countClasses()));
+					StringStats.add(String.valueOf(stats.countPackages()));
+					System.out.println("em.getFileName(): "+em.getFileName());
+					mapStats.put(em.getFileName(), StringStats);
+					ficheirosexcel.add(em.getFileName());
+				
+					
+				} catch (FileNotFoundException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				} catch (IOException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+				
+			}
+		});
+
+	
 
 		
 //		if (ficheirosexcel.getSize() != null) {
@@ -179,6 +271,11 @@ public class GuiClass extends Shell {
 		});
 		codesmells.setBounds(266, 106, 180, 30);
 		codesmells.setText("Deteção de codesmells");
+		
+
+	
+		
+		
 
 //		}
 
