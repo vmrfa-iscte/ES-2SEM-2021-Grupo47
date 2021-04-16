a9561aff034a0749b55cdeb4beae136c1fc5c1ca
alincj <alin.cj10@gmail.com>
2021-04-16 11:45:57 +0100

    HasCodeSmellTest

---
+++ Grupo47/src/test/java/G47/Grupo47/HasCodeSmellTest.java
@@ -0,0 +1,14 @@
+package G47.Grupo47;
+
+import junit.framework.TestCase;
+
+public class HasCodeSmellTest extends TestCase {
+	
+	public void testGetters() {
+		HasCodeSmell hcs = new HasCodeSmell("Alin", "Guy","Fernandinho");
+		assertEquals("Alin",hcs.getName());
+		assertEquals("Guy",hcs.getHasCodeSmell());
+		assertEquals("Fernandinho",hcs.getMethod_ID());
+	}
+
+}
