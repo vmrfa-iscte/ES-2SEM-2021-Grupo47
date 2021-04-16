7ac46f445b4c11267cc6b0a46c3504cc8bfc3b18
gtggt <gtggt@iscte-iul.pt>
2021-04-16 10:07:53 +0100

    atualização GUI

--- Grupo47/src/main/java/G47/Grupo47/mainGUI.java
+++ Grupo47/src/main/java/G47/Grupo47/mainGUI.java
@@ -21,6 +21,7 @@ import javax.swing.JOptionPane;
 import javax.swing.JTextField;
 import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
+import javax.swing.filechooser.FileNameExtensionFilter;
 
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Display;
@@ -54,6 +55,10 @@ import org.eclipse.swt.widgets.TabFolder;
 import org.eclipse.swt.widgets.Menu;
 import org.eclipse.swt.widgets.MenuItem;
 import org.eclipse.swt.custom.ScrolledComposite;
+import org.eclipse.swt.events.ControlAdapter;
+import org.eclipse.swt.events.ControlEvent;
+import org.eclipse.swt.events.GestureListener;
+import org.eclipse.swt.events.GestureEvent;
 
 public class mainGUI extends Shell {
 
@@ -91,9 +96,13 @@ public class mainGUI extends Shell {
 	private ArrayList<Metrics> actualmetrics;
 	private Label lblDefinaUmaRegra;
 	private Combo metrica2;
-	private Combo text;
 	private Label validation;
 	private Label validation1;
+	private Button btnSelecionarFicheirohistrico;
+	private Text text_1;
+	private File pastaselecionada;
+	private Button guardarhistorico;
+	private File historico;
 
 	/**
 	 * Launch the application.
@@ -168,7 +177,7 @@ public class mainGUI extends Shell {
 		ficheirosexcel.setBounds(10, 77, 345, 164);
 
 		composite = new Composite(this, SWT.NONE);
-		composite.setBounds(10, 269, 728, 385);
+		composite.setBounds(10, 257, 772, 424);
 
 		txtNmeroDeMtodos = new Text(this, SWT.BORDER);
 		txtNmeroDeMtodos.setEditable(false);
@@ -274,19 +283,19 @@ public class mainGUI extends Shell {
 				}
 			}
 		});
-		metrica1.setBounds(10, 83, 181, 28);
+		metrica1.setBounds(10, 65, 181, 28);
 		metrica1.setText("");
 		metrica1.add("LOC_method");
 		metrica1.add("WMC_class");
 
 		Combo operador = new Combo(composite, SWT.NONE);
-		operador.setBounds(298, 83, 84, 28);
+		operador.setBounds(287, 65, 84, 28);
 		operador.setText("");
 		operador.add("OR");
 		operador.add("AND");
 
 		metrica2 = new Combo(composite, SWT.NONE);
-		metrica2.setBounds(409, 83, 180, 28);
+		metrica2.setBounds(390, 65, 180, 28);
 		metrica2.setText("");
 
 		limite_1 = new Text(composite, SWT.BORDER);
@@ -302,7 +311,7 @@ public class mainGUI extends Shell {
 
 		});
 
-		limite_1.setBounds(197, 83, 84, 30);
+		limite_1.setBounds(197, 65, 84, 30);
 		limite_1.setText("Limite");
 
 		limite_2 = new Text(composite, SWT.BORDER);
@@ -317,7 +326,7 @@ public class mainGUI extends Shell {
 			}
 
 		});
-		limite_2.setBounds(612, 83, 106, 28);
+		limite_2.setBounds(599, 65, 145, 28);
 		limite_2.setText("Limite");
 
 		List regras = new List(composite, SWT.BORDER | SWT.H_SCROLL | SWT.V_SCROLL);
@@ -333,15 +342,15 @@ public class mainGUI extends Shell {
 				operador.setText(currentRule.getOperator());
 			}
 		});
-		regras.setLocation(10, 151);
-		regras.setSize(431, 188);
+		regras.setLocation(10, 144);
+		regras.setSize(431, 230);
 
 		btnDefinirRegras = new Button(composite, SWT.NONE);
 		btnDefinirRegras.addSelectionListener(new SelectionAdapter() {
 			@Override
 			public void widgetSelected(SelectionEvent e) {
 				boolean v = false;
-				if (!text.getText().isEmpty()) {
+//				if (!text_1.getText().isEmpty()) {
 					if (!metrica1.getText().isEmpty() && !operador.getText().isEmpty() && !metrica2.getText().isEmpty()
 							&& !limite_2.getText().isEmpty() && !limite_1.getText().isEmpty()
 							&& validation.getText().isEmpty() && validation1.getText().isEmpty()) {
@@ -362,31 +371,31 @@ public class mainGUI extends Shell {
 							regras.add(content);
 							list.add(rule);
 							System.out.println(list.size());
-							try {
-								FileWriter fw = new FileWriter(rules, true);
-								BufferedWriter bw = new BufferedWriter(fw);
-								System.out.println(rules.length());
-								bw.write(content);
-								bw.newLine();
-								bw.close();
-
-							} catch (IOException e1) {
-								// TODO Auto-generated catch block
-								e1.printStackTrace();
-							}
+//							try {
+//								FileWriter fw = new FileWriter(new File(text_1.getText()), true);
+//								BufferedWriter bw = new BufferedWriter(fw);
+//								System.out.println(rules.length());
+//								bw.write(content);
+//								bw.newLine();
+//								bw.close();
+//
+//							} catch (IOException e1) {
+//								// TODO Auto-generated catch block
+//								e1.printStackTrace();
+//							}
 						}
 
 					} else {
 						JOptionPane.showMessageDialog(null, "Preencha corretamente todos os campos.");
 					}
-				} else {
-					JOptionPane.showMessageDialog(null, "Selecione a pasta destino para o histórico.");
-				}
+//				} else {
+//					JOptionPane.showMessageDialog(null, "Selecione o ficheiro destino para o histórico.");
+//				}
 
 			}
 		});
 
-		btnDefinirRegras.setBounds(475, 150, 104, 30);
+		btnDefinirRegras.setBounds(451, 143, 142, 30);
 		btnDefinirRegras.setText("Definir regra");
 
 		Button alterarregra = new Button(composite, SWT.NONE);
@@ -407,19 +416,19 @@ public class mainGUI extends Shell {
 						if (x == i) {
 							regras.remove(x);
 							regras.add(update, x);
-							FileWriter fw;
-							try {
-								fw = new FileWriter(rules, true);
-								BufferedWriter bw = new BufferedWriter(fw);
-								System.out.println(rules.length());
-								bw.write(update);
-								bw.newLine();
-								bw.close();
-
-							} catch (IOException e1) {
-								// TODO Auto-generated catch block
-								e1.printStackTrace();
-							}
+//							FileWriter fw;
+//							try {
+//								fw = new FileWriter(new File(text_1.getText()), true);
+//								BufferedWriter bw = new BufferedWriter(fw);
+//								System.out.println(rules.length());
+//								bw.write(update);
+//								bw.newLine();
+//								bw.close();
+//
+//							} catch (IOException e1) {
+//								// TODO Auto-generated catch block
+//								e1.printStackTrace();
+//							}
 
 						}
 					}
@@ -431,7 +440,7 @@ public class mainGUI extends Shell {
 		});
 		;
 
-		alterarregra.setBounds(599, 150, 119, 30);
+		alterarregra.setBounds(599, 143, 145, 30);
 		alterarregra.setText("Alterar regras");
 
 		Button codesmells = new Button(composite, SWT.NONE);
@@ -488,9 +497,20 @@ public class mainGUI extends Shell {
 				regras.removeAll();
 				list.clear();
 				System.out.println(list.size());
-				if (!text.getText().isEmpty()) {
+				JFileChooser pathpasta = new JFileChooser(".txt");
+				FileNameExtensionFilter filter = new FileNameExtensionFilter("TEXT FILES", "txt", "text");
+				pathpasta.setFileFilter(filter);
+				pathpasta.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
+				int returnValue = pathpasta.showOpenDialog(null);
+
+				if (returnValue == JFileChooser.APPROVE_OPTION) {
+
+					historico = pathpasta.getSelectedFile();
+
+				}
+				if (historico.getPath().endsWith(".txt")) {
 					try {
-						FileReader reader = new FileReader(rules);
+						FileReader reader = new FileReader(new File(historico.getPath()));
 						BufferedReader bufferedReader = new BufferedReader(reader);
 						String line;
 						while ((line = bufferedReader.readLine()) != null) {
@@ -513,61 +533,94 @@ public class mainGUI extends Shell {
 						e1.printStackTrace();
 					}
 				} else {
-					JOptionPane.showMessageDialog(null, "Selecione a pasta onde se encontra o histórico");
+					JOptionPane.showMessageDialog(null, "Ficheiro inválido");
 				}
+
 			}
 		});
-		carregarhist.setBounds(6, 345, 435, 30);
+
+		carregarhist.setBounds(10, 380, 212, 30);
 		carregarhist.setText("Carregar histórico de regras");
 
-		codesmells.setBounds(475, 345, 243, 30);
+		codesmells.setBounds(451, 380, 293, 30);
 		codesmells.setText("Deteção de codesmells");
 
 		Label lblRegrasGuardadas = new Label(composite, SWT.NONE);
-		lblRegrasGuardadas.setBounds(10, 125, 155, 20);
+		lblRegrasGuardadas.setBounds(10, 118, 155, 20);
 		lblRegrasGuardadas.setText("Regras guardadas:");
 
 		lblDefinaUmaRegra = new Label(composite, SWT.NONE);
 		lblDefinaUmaRegra.setText("Defina/altere uma regra para a deteção de codesmells: ");
-		lblDefinaUmaRegra.setBounds(10, 57, 397, 20);
-
-		Button pastaregras = new Button(composite, SWT.NONE);
-
-		pastaregras.addSelectionListener(new SelectionAdapter() {
-			@Override
-			public void widgetSelected(SelectionEvent e) {
-				JFileChooser path = new JFileChooser(".");
-				path.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
-				int returnValue = path.showOpenDialog(null);
-				if (returnValue == JFileChooser.APPROVE_OPTION) {
-
-					selectedFile1 = path.getSelectedFile();
-					nomepath = selectedFile1.getAbsolutePath();
-					diretoria = selectedFile1.getPath();
-					text.add(diretoria);
-				}
-
-				text.setText(selectedFile1.getPath());
-				rules = new File(text.getText() + "\\", "rules.txt");
-			}
-		});
-		pastaregras.setBounds(418, 10, 300, 30);
-		pastaregras.setText("Definir pasta destino do histórico de regras");
-
-		text = new Combo(composite, SWT.NONE);
-		text.setBounds(10, 12, 385, 28);
+		lblDefinaUmaRegra.setBounds(10, 23, 397, 20);
 
 		validation = new Label(composite, SWT.NONE);
 		validation.setForeground(SWTResourceManager.getColor(SWT.COLOR_RED));
 		validation.setFont(SWTResourceManager.getFont("Segoe UI", 8, SWT.NORMAL));
-		validation.setBounds(197, 117, 84, 20);
+		validation.setBounds(197, 99, 84, 20);
 		validation.setText("");
 
 		validation1 = new Label(composite, SWT.NONE);
 		validation1.setText("");
 		validation1.setForeground(SWTResourceManager.getColor(SWT.COLOR_RED));
 		validation1.setFont(SWTResourceManager.getFont("Segoe UI", 8, SWT.NORMAL));
-		validation1.setBounds(612, 117, 84, 20);
+		validation1.setBounds(599, 99, 145, 20);
+
+//		Button selecionarFicheirohistrico = new Button(composite, SWT.NONE);
+//		selecionarFicheirohistrico.addSelectionListener(new SelectionAdapter() {
+//			@Override
+//			public void widgetSelected(SelectionEvent e) {
+//				JFileChooser pathpasta = new JFileChooser(".");
+//				pathpasta.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+//				int returnValue = pathpasta.showOpenDialog(null);
+//
+//				if (returnValue == JFileChooser.APPROVE_OPTION) {
+//
+//					pastaselecionada = pathpasta.getSelectedFile();
+//				}
+//				if (pastaselecionada.getPath().endsWith(".txt")) {
+//					text_1.setText(pastaselecionada.getPath());
+//				} else {
+//					JOptionPane.showMessageDialog(null, "Ficheiro inválido");
+//				}
+//			}
+//		});
+//
+//		selecionarFicheirohistrico.setBounds(10, 33, 310, 30);
+//		selecionarFicheirohistrico.setText("Selecionar ficheiro 'histórico de regras'");
+
+//		text_1 = new Text(composite, SWT.BORDER);
+//		text_1.setBounds(347, 35, 397, 28);
+
+		guardarhistorico = new Button(composite, SWT.NONE);
+		guardarhistorico.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				if (!list.isEmpty()) {
+					Historico hist= new Historico(display,regras,list);
+//					hist.loadGUI();
+					for (int y = 0; y < list.size(); y++) {
+						FileWriter fw;
+						try {
+							fw = new FileWriter(new File(text_1.getText()), true);
+							BufferedWriter bw = new BufferedWriter(fw);
+							System.out.println(rules.length());
+							bw.write(list.get(y).toString());
+							bw.newLine();
+							bw.close();
+
+						} catch (IOException e1) {
+							// TODO Auto-generated catch block
+							e1.printStackTrace();
+						}
+
+					}
+				} else {
+					JOptionPane.showMessageDialog(null, "Lista de regras vazia");
+				}
+			}
+		});
+		guardarhistorico.setBounds(228, 380, 214, 30);
+		guardarhistorico.setText("Guardar histórico");
 
 		Label lblProjetoJavaescolha = new Label(this, SWT.NONE);
 		lblProjetoJavaescolha.setFont(SWTResourceManager.getFont("Segoe UI", 9, SWT.NORMAL));
@@ -596,11 +649,12 @@ public class mainGUI extends Shell {
 		mntmInformaoSobreMtricas.addSelectionListener(new SelectionAdapter() {
 			@Override
 			public void widgetSelected(SelectionEvent e) {
+				HelpGUIMetrics hgm = new HelpGUIMetrics(display);
+				hgm.loadGUI();
 
 			}
 		});
 		mntmInformaoSobreMtricas.setText("Informação sobre métricas");
-
 		createContents();
 	}
 
@@ -609,7 +663,7 @@ public class mainGUI extends Shell {
 	 */
 	protected void createContents() {
 		setText("Interface gráfica- Grupo 47");
-		setSize(766, 736);
+		setSize(810, 768);
 
 	}
 
