ea136312291c89ba9e085450ba9a87717a4b188e
gtggt <gtggt@iscte-iul.pt>
2021-04-18 19:06:27 +0100

    Help- botão de ajuda à interface

--- Grupo47/src/main/java/G47/Grupo47/HelpInterface.java
+++ Grupo47/src/main/java/G47/Grupo47/HelpInterface.java
@@ -38,11 +38,11 @@ public class HelpInterface extends Shell {
 		
 		Label extrairm = new Label(this, SWT.NONE | SWT.BORDER | SWT.WRAP);
 		extrairm.setBounds(10, 21, 870, 48);
-		extrairm.setText("1-Selecione a pasta onde se localiza o projeto do qual pretende extrair métricas, clicando no botão 'Selecionar pasta'.De seguida, pressione o botão 'Extrair métricas', do qual resultará um ficheiro com as respetivas métricas calculadas.");
+		extrairm.setText("1-Selecione a pasta onde se localiza o projeto do qual pretende extrair métricas, clicando no botão 'Selecionar pasta'. De seguida, pressione o botão 'Extrair métricas', do qual resultará um ficheiro com as respetivas métricas calculadas.");
 		
 		Label resultadosext = new Label(this, SWT.BORDER | SWT.WRAP);
 		resultadosext.setBounds(10, 75, 870, 66);
-		resultadosext.setText("2-Pode agora verificar que, na lista centrada à esqueda da interface, foi gerada uma lista com o nome do(s) ficheiro(s) excel gerado(s).Ao clicar sobre esse ficheiro, poderá verificar as características gerais do mesmo, nos campos localizados à direita da interface. ");
+		resultadosext.setText("2-Pode agora verificar que, na lista centrada à esqueda da interface, foi gerada uma lista com o nome do(s) ficheiro(s) excel gerado(s). Ao clicar sobre esse ficheiro, poderá verificar as características gerais do mesmo, nos campos localizados à direita da interface. ");
 		
 		Label regras = new Label(this, SWT.BORDER | SWT.WRAP);
 		regras.setBounds(10, 147, 870, 66);
@@ -50,7 +50,15 @@ public class HelpInterface extends Shell {
 		
 		Label alterarregra = new Label(this, SWT.BORDER | SWT.WRAP);
 		alterarregra.setBounds(10, 219, 870, 66);
-		alterarregra.setText("New Label");
+		alterarregra.setText("4- Há medida que as regras são definidas, poderá notar que as mesmas são guardadas numa lista localizada no canto inferior esquerdo da interface. Se pretender alterar alguma destas regras, basta seleciona-la, alterar os campos desejados no mesmo sitio onde os definiu e, após isto, clicar no botão 'Alterar regra'.");
+		
+		Label historico = new Label(this, SWT.BORDER | SWT.WRAP);
+		historico.setBounds(10, 291, 870, 113);
+		historico.setText("5-Após a definição das regras desejadas, tem a possibilidade de as guardar num histórico não volátil. Para tal, basta clicar no botão 'Guardar histórico', da qual resultará a abertura de uma nova interface. Nesta nova interface, terá duas possibilidades: Se já tiver um ficheiro histórico criado, basta selecionar o mesmo através do botão 'Selecionar ficheiro'. Caso pretenda criar um ficheiro novo, basta preencher os campos relativos ao nome do ficheiro e à localização destino do mesmo. Por fim, basta pressionar o botão 'Criar ficheiro histórico'.");
+		
+		Label detecaocs = new Label(this, SWT.BORDER | SWT.WRAP);
+		detecaocs.setBounds(10, 410, 870, 91);
+		detecaocs.setText("6- Por fim, temos a deteção dos codesmells. Para tal, basta selecionar uma das regras resultantes da sua definição, para a qual pretende fazer esta mesma deteção. Após esta seleção, pressione o botão 'Deteção de codesmells', da qual resultará uma nova interface com os resultados obtidos.");
 		
 		
 		
@@ -77,7 +85,7 @@ public class HelpInterface extends Shell {
 	 */
 	protected void createContents() {
 		setText("Ajuda-Utilização da interface");
-		setSize(934, 496);
+		setSize(934, 558);
 
 	}
 	
