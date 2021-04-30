b9d30f3070bb0ceca2e9ab46d82a1f1bbbc8d755
tasmm-iscte <tasmm@iscte-iul.pt>
2021-04-30 16:16:32 +0100

    JavaDoc classe DetectionChooser

--- Grupo47/src/main/java/detection/DetectionChooser.java
+++ Grupo47/src/main/java/detection/DetectionChooser.java
@@ -6,14 +6,23 @@ import classes.HasCodeSmell;
 import classes.MethodMetrics;
 import classes.Rule;
 
+/**
+ *  Esta classe tem por objetivo decidir qual a deteção adequada segundo uma regra recebida, após decisão vai efetuar a deteção
+	 e retornar uma lista com os resultados
+ * @author Tomás Mendes
+ * @version
+ *
+ */
 public class DetectionChooser {
-	
-	/* Esta classe tem por objetivo decidir qual a deteção adequada segundo uma regra recebida, após decisão vai efetuar a deteção
-	 * e retornar uma lista com os resultados
-	 */
+	 
 	
 	private static final String GREATER_THAN = ">", LESSER_THAN = "<";
 	
+	/** Decide qual a vertente da regra a aplicar consoante os sinais, aplica a vertente correta e retorna os resultados
+	 * @param ruleReceived regra recebida
+	 * @param actualmetrics Array com metodos e suas respetivas metricas
+	 * @return	ArrayList com resultados de detecao
+	 */
 	public ArrayList<HasCodeSmell> chooseDetectionLocMethod(Rule ruleReceived,ArrayList<MethodMetrics> actualmetrics) {
 		CodeSmellsDetector detector = new CodeSmellsDetector(actualmetrics,ruleReceived);
 		ArrayList<HasCodeSmell> hasCodeSmellResult = new ArrayList<>();
@@ -26,6 +35,12 @@ public class DetectionChooser {
 		return hasCodeSmellResult;
 	}
 	
+	/**
+ 	Decide qual a vertente da regra a aplicar consoante os sinais, aplica a vertente correta e retorna os resultados
+	  @param ruleReceived regra recebida
+	  @param actualmetrics Array com metodos e suas respetivas metricas
+	  @return	ArrayList com resultados de detecao
+	 */
 	public ArrayList<HasCodeSmell> chooseDetectionWMC_NOM(Rule ruleReceived,ArrayList<MethodMetrics> actualmetrics) {
 		ArrayList<HasCodeSmell> hasCodeSmellResult = new ArrayList<>();
 		CodeSmellsDetector detector2 = new CodeSmellsDetector(actualmetrics,ruleReceived);
@@ -38,6 +53,12 @@ public class DetectionChooser {
 		return hasCodeSmellResult;
 	}
 	
+	/**
+	 Decide qual a vertente da regra a aplicar consoante os sinais, aplica a vertente correta e retorna os resultados
+	  @param ruleReceived regra recebida
+	  @param actualmetrics Array com metodos e suas respetivas metricas
+	  @return	ArrayList com resultados de detecao
+	 */
 	public ArrayList<HasCodeSmell> chooseDetectionWMC_LOC(Rule ruleReceived,ArrayList<MethodMetrics> actualmetrics) {
 		ArrayList<HasCodeSmell> hasCodeSmellResult = new ArrayList<>();
 		CodeSmellsDetector detector2 = new CodeSmellsDetector(actualmetrics,ruleReceived);
@@ -50,6 +71,12 @@ public class DetectionChooser {
 		return hasCodeSmellResult;
 	}
 	
+	/**
+	 Decide qual a vertente da regra a aplicar consoante os sinais, aplica a vertente correta e retorna os resultados
+	  @param ruleReceived regra recebida
+	  @param actualmetrics Array com metodos e suas respetivas metricas
+	  @return	ArrayList com resultados de detecao
+	 */
 	public ArrayList<HasCodeSmell> chooseDetectionNOM_LOC(Rule ruleReceived,ArrayList<MethodMetrics> actualmetrics) {
 		ArrayList<HasCodeSmell> hasCodeSmellResult = new ArrayList<>();
 		CodeSmellsDetector detector2 = new CodeSmellsDetector(actualmetrics,ruleReceived);
@@ -62,6 +89,12 @@ public class DetectionChooser {
 		return hasCodeSmellResult;
 	}
 
+	/**
+ Decide qual a vertente da regra a aplicar consoante os sinais, aplica a vertente correta e retorna os resultados
+	  @param ruleReceived regra recebida
+	  @param actualmetrics Array com metodos e suas respetivas metricas
+	  @return	ArrayList com resultados de detecao
+	 */
 	public ArrayList<HasCodeSmell> chooseDetectionWMC_NOM_LOC(Rule ruleReceived,ArrayList<MethodMetrics> actualmetrics) {
 		ArrayList<HasCodeSmell> hasCodeSmellResult = new ArrayList<>();
 		CodeSmellsDetector detector = new CodeSmellsDetector(actualmetrics, ruleReceived);
@@ -87,41 +120,97 @@ public class DetectionChooser {
 		return hasCodeSmellResult;
 	}
 	
+	/**
+	 * Retorna se um conjunto de 3 sinais de uma dada regra correspondem a >,>,>
+	 * @param signal1
+	 * @param signal2
+	 * @param signal3
+	 * @return booleano que indica se sim ou se nao
+	 */
 	private boolean isGreaterGreaterGreater(String signal1, String signal2, String signal3) {
 		return (signal1.equals(GREATER_THAN) && signal2.equals(GREATER_THAN) && signal3.equals(GREATER_THAN)) ;
 		
 	}
 	
+	/**
+	 * Retorna se um conjunto de 3 sinais de uma dada regra correspondem a <,<,<
+	 * @param signal1
+	 * @param signal2
+	 * @param signal3
+	 * @return booleano que indica se sim ou se nao
+	 */
 	private boolean isLesserLesserLesser(String firstSignal,String secondSignal, String thirdSignal) {
 		return (firstSignal.equals(LESSER_THAN) && secondSignal.equals(LESSER_THAN) && thirdSignal.equals(LESSER_THAN)) ;
 		
 	}
 	
+	/**
+	 * Retorna se um conjunto de 3 sinais de uma dada regra correspondem a >,<,<
+	 * @param signal1
+	 * @param signal2
+	 * @param signal3
+	 * @return booleano que indica se sim ou se nao
+	 */
 	private boolean isGreaterLesserLesser(String firstSignal,String secondSignal, String thirdSignal) {
 		return (firstSignal.equals(GREATER_THAN) && secondSignal.equals(LESSER_THAN) && thirdSignal.equals(LESSER_THAN)) ;
 		
 	}
 	
+	/**
+	 * Retorna se um conjunto de 3 sinais de uma dada regra correspondem a >,>,<
+	 * @param signal1
+	 * @param signal2
+	 * @param signal3
+	 * @return booleano que indica se sim ou se nao
+	 */
 	private boolean isGreaterGreaterLesser(String firstSignal,String secondSignal, String thirdSignal) {
 		return (firstSignal.equals(GREATER_THAN) && secondSignal.equals(GREATER_THAN) && thirdSignal.equals(LESSER_THAN)) ;
 		
 	}
 	
+	/**
+		 * Retorna se um conjunto de 3 sinais de uma dada regra correspondem a >,<,>
+	 * @param signal1
+	 * @param signal2
+	 * @param signal3
+	 * @return booleano que indica se sim ou se nao
+	 */
 	private boolean isGreaterLesserGreater(String firstSignal,String secondSignal, String thirdSignal) {
 		return (firstSignal.equals(GREATER_THAN) && secondSignal.equals(LESSER_THAN) && thirdSignal.equals(GREATER_THAN)) ;
 		
 	}
 	
+	/**
+	 * Retorna se um conjunto de 3 sinais de uma dada regra correspondem a <,<,>
+	 * @param signal1
+	 * @param signal2
+	 * @param signal3
+	 * @return booleano que indica se sim ou se nao
+	 */
 	private boolean isLesserLesserGreater(String firstSignal,String secondSignal, String thirdSignal) {
 		return (firstSignal.equals(LESSER_THAN) && secondSignal.equals(LESSER_THAN) && thirdSignal.equals(GREATER_THAN)) ;
 		
 	}
 	
+	/**
+	 * Retorna se um conjunto de 3 sinais de uma dada regra correspondem a <,>,>
+	 * @param signal1
+	 * @param signal2
+	 * @param signal3
+	 * @return booleano que indica se sim ou se nao
+	 */
 	private boolean isLesserGreaterGreater(String firstSignal,String secondSignal, String thirdSignal) {
 		return (firstSignal.equals(LESSER_THAN) && secondSignal.equals(GREATER_THAN) && thirdSignal.equals(GREATER_THAN)) ;
 		
 	}
 	
+	/**
+	 * Retorna se um conjunto de 3 sinais de uma dada regra correspondem a <,>,<
+	 * @param signal1
+	 * @param signal2
+	 * @param signal3
+	 * @return booleano que indica se sim ou se nao
+	 */
 	private boolean isLesserGreaterLesser(String firstSignal,String secondSignal, String thirdSignal) {
 		return (firstSignal.equals(LESSER_THAN) && secondSignal.equals(GREATER_THAN) && thirdSignal.equals(LESSER_THAN)) ;
 		
