16de13812fe84795ccc983485174678c26d871a4
gtggt <gtggt@iscte-iul.pt>
2021-04-30 10:30:26 +0100

    atualização junit

--- Grupo47/src/test/java/detection/CheckRuleCombinationsTest.java
+++ Grupo47/src/test/java/detection/CheckRuleCombinationsTest.java
@@ -15,13 +15,30 @@ class CheckRuleCombinationsTest {
 	private MethodMetrics mm;
 	private boolean v;
 	private boolean f;
+	private CheckRuleCombinations combination;
+	private CheckRuleCombinations combination1;
+	private CheckRuleCombinations combination2;
+	private CheckRuleCombinations combination3;
+	private CheckRuleCombinations combination4;
+	private CheckRuleCombinations combination5;
+	private CheckRuleCombinations combination6;
 
+	/**
+	 * @throws Exception
+	 */
 	@BeforeEach
 	void setUp() throws Exception {
 		mm = new MethodMetrics(new MethodIdentity("methodName", "className", "packageName", 0),
 				new Metrics(5, 5, 5, 5, 5));
 		v = true;
 		f = false;
+		 combination = new CheckRuleCombinations(6, 4, 6);// VVV
+		 combination1 = new CheckRuleCombinations(4, 4, 6);// FVV
+		 combination2 = new CheckRuleCombinations(4, 6, 6);// FFV
+		 combination3 = new CheckRuleCombinations(4, 6, 4);// FFF
+		 combination4 = new CheckRuleCombinations(6, 6, 4);// VFF
+		 combination5 = new CheckRuleCombinations(6, 4, 4);// VVF
+		 combination6 = new CheckRuleCombinations(4, 4, 4); // FVF
 	}
 
 	@AfterEach
@@ -30,118 +47,85 @@ class CheckRuleCombinationsTest {
 
 	@Test
 	void testIsGodLesserGreaterLesser_AA() {
-		CheckRuleCombinations crb = new CheckRuleCombinations(6, 4, 6);// VVV
-		CheckRuleCombinations crb1 = new CheckRuleCombinations(4, 4, 6);// FVV
-		CheckRuleCombinations crb2 = new CheckRuleCombinations(4, 6, 6);// FFV
-		CheckRuleCombinations crb3 = new CheckRuleCombinations(4, 6, 4);// FFF
-		CheckRuleCombinations crb4 = new CheckRuleCombinations(6, 6, 4);// VFF
-		CheckRuleCombinations crb5 = new CheckRuleCombinations(6, 4, 4);// VVF
-		assertNotNull(crb);
-		assertNotNull(crb1);
-		assertNotNull(crb2);
-		assertNotNull(crb3);
-		assertNotNull(crb4);
-		assertNotNull(crb5);
-		assertEquals(v, crb.isGodLesserGreaterLesser_AndAnd(mm));
-		assertEquals(f, crb1.isGodLesserGreaterLesser_AndAnd(mm));
-		assertEquals(f, crb2.isGodLesserGreaterLesser_AndAnd(mm));
-		assertEquals(f, crb3.isGodLesserGreaterLesser_AndAnd(mm));
-		assertEquals(f, crb4.isGodLesserGreaterLesser_AndAnd(mm));
-		assertEquals(f, crb5.isGodLesserGreaterLesser_AndAnd(mm));
+		assertNotNull(combination);
+		assertNotNull(combination1);
+		assertNotNull(combination2);
+		assertNotNull(combination3);
+		assertNotNull(combination4);
+		assertNotNull(combination5);
+		assertEquals(v, combination.isGodLesserGreaterLesser_AndAnd(mm));
+		assertEquals(f, combination1.isGodLesserGreaterLesser_AndAnd(mm));
+		assertEquals(f, combination2.isGodLesserGreaterLesser_AndAnd(mm));
+		assertEquals(f, combination3.isGodLesserGreaterLesser_AndAnd(mm));
+		assertEquals(f, combination4.isGodLesserGreaterLesser_AndAnd(mm));
+		assertEquals(f, combination5.isGodLesserGreaterLesser_AndAnd(mm));
 
 	}
 
 	@Test
 	void testisGodLesserGreaterLesser_AndOr() {
-		CheckRuleCombinations crb = new CheckRuleCombinations(6, 4, 6);// VVV
-		CheckRuleCombinations crb1 = new CheckRuleCombinations(4, 4, 6);// FVV
-		CheckRuleCombinations crb2 = new CheckRuleCombinations(4, 6, 6);// FFV
-		CheckRuleCombinations crb3 = new CheckRuleCombinations(4, 6, 4);// FFF
-		CheckRuleCombinations crb4 = new CheckRuleCombinations(6, 6, 4);// VFF
-		CheckRuleCombinations crb5 = new CheckRuleCombinations(6, 4, 4);// VVF
-		assertNotNull(crb);
-		assertNotNull(crb1);
-		assertNotNull(crb2);
-		assertNotNull(crb3);
-		assertNotNull(crb4);
-		assertNotNull(crb5);
-		assertEquals(v, crb.isGodLesserGreaterLesser_AndOr(mm));
-		assertEquals(v, crb1.isGodLesserGreaterLesser_AndOr(mm));
-		assertEquals(v, crb2.isGodLesserGreaterLesser_AndOr(mm));
-		assertEquals(f, crb3.isGodLesserGreaterLesser_AndOr(mm));
-		assertEquals(f, crb4.isGodLesserGreaterLesser_AndOr(mm));
-		assertEquals(v, crb5.isGodLesserGreaterLesser_AndOr(mm));
+		assertNotNull(combination);
+		assertNotNull(combination1);
+		assertNotNull(combination2);
+		assertNotNull(combination3);
+		assertNotNull(combination4);
+		assertNotNull(combination5);
+		assertEquals(v, combination.isGodLesserGreaterLesser_AndOr(mm));
+		assertEquals(v, combination1.isGodLesserGreaterLesser_AndOr(mm));
+		assertEquals(v, combination2.isGodLesserGreaterLesser_AndOr(mm));
+		assertEquals(f, combination3.isGodLesserGreaterLesser_AndOr(mm));
+		assertEquals(f, combination4.isGodLesserGreaterLesser_AndOr(mm));
+		assertEquals(v, combination5.isGodLesserGreaterLesser_AndOr(mm));
 
 	}
 
 	@Test
 	void isGodLesserGreaterLesser_OrAnd() {
-		CheckRuleCombinations crb = new CheckRuleCombinations(6, 4, 6);// VVV
-		CheckRuleCombinations crb1 = new CheckRuleCombinations(4, 4, 6);// FVV
-		CheckRuleCombinations crb2 = new CheckRuleCombinations(4, 6, 6);// FFV
-		CheckRuleCombinations crb3 = new CheckRuleCombinations(4, 6, 4);// FFF
-		CheckRuleCombinations crb4 = new CheckRuleCombinations(6, 6, 4);// VFF
-		CheckRuleCombinations crb5 = new CheckRuleCombinations(6, 4, 4);// VVF
-		CheckRuleCombinations crb6 = new CheckRuleCombinations(4, 4, 4); // FVF
-		assertNotNull(crb);
-		assertNotNull(crb1);
-		assertNotNull(crb2);
-		assertNotNull(crb3);
-		assertNotNull(crb4);
-		assertNotNull(crb5);
-		assertNotNull(crb6);
-		assertEquals(v, crb.isGodLesserGreaterLesser_OrAnd(mm));
-		assertEquals(v, crb1.isGodLesserGreaterLesser_OrAnd(mm));
-		assertEquals(f, crb2.isGodLesserGreaterLesser_OrAnd(mm));
-		assertEquals(f, crb3.isGodLesserGreaterLesser_OrAnd(mm));
-		assertEquals(v, crb4.isGodLesserGreaterLesser_OrAnd(mm));
-		assertEquals(v, crb5.isGodLesserGreaterLesser_OrAnd(mm));
-		assertEquals(f, crb6.isGodLesserGreaterLesser_OrAnd(mm));
+		assertNotNull(combination);
+		assertNotNull(combination1);
+		assertNotNull(combination2);
+		assertNotNull(combination3);
+		assertNotNull(combination4);
+		assertNotNull(combination5);
+		assertNotNull(combination6);
+		assertEquals(v, combination.isGodLesserGreaterLesser_OrAnd(mm));
+		assertEquals(v, combination1.isGodLesserGreaterLesser_OrAnd(mm));
+		assertEquals(f, combination2.isGodLesserGreaterLesser_OrAnd(mm));
+		assertEquals(f, combination3.isGodLesserGreaterLesser_OrAnd(mm));
+		assertEquals(v, combination4.isGodLesserGreaterLesser_OrAnd(mm));
+		assertEquals(v, combination5.isGodLesserGreaterLesser_OrAnd(mm));
+		assertEquals(f, combination6.isGodLesserGreaterLesser_OrAnd(mm));
 
 	}
 
 	@Test
 	void testIsGodLesserGreaterLesser_OrOr() {
-		CheckRuleCombinations crb = new CheckRuleCombinations(6, 4, 6);// VVV
-		CheckRuleCombinations crb1 = new CheckRuleCombinations(4, 4, 6);// FVV
-		CheckRuleCombinations crb2 = new CheckRuleCombinations(4, 6, 6);// FFV
-		CheckRuleCombinations crb3 = new CheckRuleCombinations(4, 6, 4);// FFF
-		CheckRuleCombinations crb4 = new CheckRuleCombinations(6, 6, 4);// VFF
-		CheckRuleCombinations crb5 = new CheckRuleCombinations(6, 4, 4);// VVF
-		CheckRuleCombinations crb6 = new CheckRuleCombinations(4, 4, 4); // FVF
-		assertNotNull(crb);
-		assertNotNull(crb1);
-		assertNotNull(crb2);
-		assertNotNull(crb3);
-		assertNotNull(crb4);
-		assertNotNull(crb5);
-		assertNotNull(crb6);
-		assertEquals(v, crb.isGodLesserGreaterLesser_OrOr(mm));
-		assertEquals(v, crb1.isGodLesserGreaterLesser_OrOr(mm));
-		assertEquals(v, crb2.isGodLesserGreaterLesser_OrOr(mm));
-		assertEquals(f, crb3.isGodLesserGreaterLesser_OrOr(mm));
-		assertEquals(v, crb4.isGodLesserGreaterLesser_OrOr(mm));
-		assertEquals(v, crb5.isGodLesserGreaterLesser_OrOr(mm));
-		assertEquals(v, crb6.isGodLesserGreaterLesser_OrOr(mm));
+		assertNotNull(combination);
+		assertNotNull(combination1);
+		assertNotNull(combination2);
+		assertNotNull(combination3);
+		assertNotNull(combination4);
+		assertNotNull(combination5);
+		assertNotNull(combination6);
+		assertEquals(v, combination.isGodLesserGreaterLesser_OrOr(mm));
+		assertEquals(v, combination1.isGodLesserGreaterLesser_OrOr(mm));
+		assertEquals(v, combination2.isGodLesserGreaterLesser_OrOr(mm));
+		assertEquals(f, combination3.isGodLesserGreaterLesser_OrOr(mm));
+		assertEquals(v, combination4.isGodLesserGreaterLesser_OrOr(mm));
+		assertEquals(v, combination5.isGodLesserGreaterLesser_OrOr(mm));
+		assertEquals(v, combination6.isGodLesserGreaterLesser_OrOr(mm));
 
 	}
 
 	@Test
 	void testisGodLesserGreaterGreater_AndAnd() {
-		CheckRuleCombinations crb = new CheckRuleCombinations(6, 4, 6);// VVV
-		CheckRuleCombinations crb1 = new CheckRuleCombinations(4, 4, 6);// FVV
-		CheckRuleCombinations crb2 = new CheckRuleCombinations(4, 6, 6);// FFV
-		CheckRuleCombinations crb3 = new CheckRuleCombinations(4, 6, 4);// FFF
-		CheckRuleCombinations crb4 = new CheckRuleCombinations(6, 6, 4);// VFF
-		CheckRuleCombinations crb5 = new CheckRuleCombinations(6, 4, 4);// VVF
-		CheckRuleCombinations crb6 = new CheckRuleCombinations(4, 4, 4); // FVF
-		assertNotNull(crb);
-		assertNotNull(crb1);
-		assertNotNull(crb2);
-		assertNotNull(crb3);
-		assertNotNull(crb4);
-		assertNotNull(crb5);
-		assertNotNull(crb6);
+		assertNotNull(combination);
+		assertNotNull(combination1);
+		assertNotNull(combination2);
+		assertNotNull(combination3);
+		assertNotNull(combination4);
+		assertNotNull(combination5);
+		assertNotNull(combination6);
 		//tou a acabar este
 		
 	
