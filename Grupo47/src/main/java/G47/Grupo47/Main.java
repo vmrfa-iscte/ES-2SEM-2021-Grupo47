0d6445e339ddbca4763c1a33d521b970ced6157d
tasmm-iscte <tasmm@iscte-iul.pt>
2021-05-01 14:33:16 +0100

    JavaDoc Main

--- Grupo47/src/main/java/G47/Grupo47/Main.java
+++ Grupo47/src/main/java/G47/Grupo47/Main.java
@@ -8,15 +8,31 @@ import gui.mainGUI;
 
 public class Main {
 
+	/** Funcao main que permite correr todo o projeto
+	 * @param args valor default do java
+	 * @throws IOException excecao
+	 * @author Tomas Mendes
+	 * @author Vasco Fontoura
+	 * @author Gwinie tha pooh, aka gwin diesel, aka damn negro
+	 * 
+	 * 
+	 */
 	public static void main(String[] args) throws IOException {
 		try {
+			// Criacao de um Display
 			Display display = Display.getDefault();
+		
+			// Criacao da GUI principal do nosso projeto
 			mainGUI shell = new mainGUI(display);
+			
 			shell.open();
 			shell.layout();
+			// Abertura e preenchimento do layout da GUI
 			while (!shell.isDisposed()) {
+				// Enquanto a shell nao e fechada
 				if (!display.readAndDispatch()) {
 					display.sleep();
+					// o display mantem se aberto
 				}
 			}
 		} catch (Exception e) {
